aee4920b324069e49f7b9898b078c514ec445f5d
mashaal <omar@teacups.io>
2021-08-13 06:22:53 +1000

    update and pin deps

--- examples/boilerplate/server.js
+++ examples/boilerplate/server.js
@@ -1,4 +1,4 @@
-import ultra from "https://deno.land/x/ultra/mod.js";
+import ultra from "https://deno.land/x/ultra@v0.1/mod.js";
 
 await ultra({
   importmap: await Deno.readTextFile("importmap.json"),
--- examples/ultra-website/server.js
+++ examples/ultra-website/server.js
@@ -1,4 +1,4 @@
-import ultra, { router } from "https://deno.land/x/ultra/mod.js";
+import ultra, { router } from "https://deno.land/x/ultra@v0.1/mod.js";
 import { join } from "https://deno.land/std@0.104.0/path/mod.ts";
 
 router.get("/markdown", async (context) => {
