b0cdd428e21864eb140fd2a16102e15b72a71525
mashaal <omar@teacups.io>
2021-08-30 20:09:22 +1000

    update ultra website

--- examples/ultra-website/public/index.jsx
+++ examples/ultra-website/public/index.jsx
@@ -14,8 +14,12 @@ const Index = () => {
   const { content } = data;
   return (
     <main>
-     {/* <Ghost />*/}
-      <img className="logo" src="https://dweb.link/ipfs/bafkreiah6lyqltjzmqaggn3iang6sip7tnbotvxyqeg6zgrem6wqniegfm" height="350" />
+      <Ghost />
+      <img
+        className="logo"
+        src="https://dweb.link/ipfs/bafkreiah6lyqltjzmqaggn3iang6sip7tnbotvxyqeg6zgrem6wqniegfm"
+        height="350"
+      />
       <h1>Ultra</h1>
       <h2>Deno + React: No build, No bundle, All streaming</h2>
       <a className="gh" target="_blank">View on GitHub</a>
