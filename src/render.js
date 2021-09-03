55a3c891a331fc5bca779fc184a82e3ab9dc931e
Je Xia <i@jex.me>
2021-09-03 09:12:12 +0800

    Remove repeated import

--- src/render.js
+++ src/render.js
@@ -1,7 +1,6 @@
 import { join } from "https://deno.land/std@0.106.0/path/mod.ts";
 import React from "react";
 import ReactDOM from "react-dom/server";
-import { join } from "https://deno.land/std@0.106.0/path/mod.ts";
 import { Router } from "wouter";
 import { HelmetProvider } from "helmet";
 
