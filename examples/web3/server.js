a3c7b2d1dc3dcc5420e0e6a5ef83e9c771b6112c
Omar Mashaal <omar@teacups.io>
2022-01-29 13:40:22 +1100

    fix ultra server for examples

--- examples/web3/server.js
+++ examples/web3/server.js
@@ -1,4 +1,5 @@
-import ultra from "https://deno.land/x/ultra@v0.6/mod.ts";
+// import ultra from "https://deno.land/x/ultra@v0.6/mod.ts";
+import ultra from "../../mod.ts";
 
 ultra({
   importmap: await Deno.readTextFile("importmap.json"),
