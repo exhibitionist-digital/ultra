f471632759efbad0d0a0481484ac398f7089bc37
Omar Mashaal <omar@teacups.io>
2022-02-14 15:31:42 +1100

    remove vercel from 0.7.0

--- src/vercel/deps.ts
+++
@@ -1,9 +0,0 @@
-export { walk } from "https://deno.land/std@0.123.0/fs/mod.ts";
-export { concat } from "https://deno.land/std@0.123.0/bytes/mod.ts";
-export { join } from "https://deno.land/std@0.123.0/path/mod.ts";
-export { Buffer } from "https://deno.land/std@0.123.0/io/mod.ts";
-export { serve } from "https://deno.land/std@0.123.0/http/server.ts";
-export { createCache } from "https://deno.land/x/deno_cache/mod.ts";
-export { createGraph } from "https://deno.land/x/deno_graph@0.22.0/mod.ts";
-export { extname } from "https://deno.land/std@0.123.0/path/mod.ts";
-export { default as mime } from "https://esm.sh/mime-types";
