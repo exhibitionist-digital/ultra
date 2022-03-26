37ced4299d64968812fdab8c1b06d523078648b2
Omar Mashaal <omar@teacups.io>
2022-03-27 02:51:43 +1100

    update deps

--- src/deno/deps.ts
+++ src/deno/deps.ts
@@ -1,8 +1,8 @@
-export { walk } from "https://deno.land/std@0.123.0/fs/mod.ts";
-export { concat } from "https://deno.land/std@0.123.0/bytes/mod.ts";
-export { join } from "https://deno.land/std@0.123.0/path/mod.ts";
-export { Buffer } from "https://deno.land/std@0.123.0/io/mod.ts";
-export { serve } from "https://deno.land/std@0.123.0/http/server.ts";
-export { readableStreamFromReader } from "https://deno.land/std@0.123.0/streams/conversion.ts";
-export { extname } from "https://deno.land/std@0.123.0/path/mod.ts";
-export { default as mime } from "https://esm.sh/mime-types";
+export { walk } from "https://deno.land/std@0.132.0/fs/mod.ts";
+export { concat } from "https://deno.land/std@0.132.0/bytes/mod.ts";
+export { join } from "https://deno.land/std@0.132.0/path/mod.ts";
+export { Buffer } from "https://deno.land/std@0.132.0/io/mod.ts";
+export { serve } from "https://deno.land/std@0.132.0/http/server.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.132.0/streams/conversion.ts";
+export { extname } from "https://deno.land/std@0.132.0/path/mod.ts";
+export { default as mime } from "https://esm.sh/mime-types@2.1.35";
