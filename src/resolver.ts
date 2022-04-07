3738050f40418fd6d77868f1611f340e553fb13c
Omar Mashaal <omar@teacups.io>
2022-04-07 12:35:47 +1000

    clean up hashing method

--- src/resolver.ts
+++ src/resolver.ts
@@ -25,6 +25,8 @@ export const isValidURL = (url: string) => {
 };
 
 export const hashFile = (url: string) => {
+  // strip query params from hashing
+  url = url.split("?")[0];
   const msgUint8 = new TextEncoder().encode(url);
   const hashBuffer = crypto.subtle.digestSync("SHA-256", msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
