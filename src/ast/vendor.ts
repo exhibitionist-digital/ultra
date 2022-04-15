a455591ae1d47a069e28a4d0fa0d165a94db86a1
James Edmonds <james@deckchairlabs.com>
2022-04-15 07:37:31 +0000

    dry vendor visitor

--- src/ast/vendor.ts
+++ src/ast/vendor.ts
@@ -2,6 +2,7 @@ import {
   ExportAllDeclaration,
   ExportNamedDeclaration,
   ImportDeclaration,
+  StringLiteral,
   Visitor,
 } from "../deps.ts";
 import { hashFile } from "../resolver.ts";
@@ -10,30 +11,13 @@ const prefix = "./";
 
 export class VendorVisitor extends Visitor {
   visitExportAllDeclaration(node: ExportAllDeclaration) {
-    const { value } = node.source;
-    const url = new URL(value);
-
-    node.source.value = `${
-      prefix + hashFile(value.replace(url.origin, ""))
-    }.js`;
-
-    //@ts-ignore StringLiteral missing raw field
-    node.source.raw = `"${node.source.value}"`;
-
+    node.source = this.replaceImportStringLiteral(node.source);
     return super.visitExportAllDeclaration(node);
   }
 
   visitExportNamedDeclaration(node: ExportNamedDeclaration) {
     if (node.source) {
-      const { value } = node.source;
-      const url = new URL(value);
-
-      node.source.value = `${
-        prefix + hashFile(value.replace(url.origin, ""))
-      }.js`;
-
-      //@ts-ignore StringLiteral missing raw field
-      node.source.raw = `"${node.source.value}"`;
+      node.source = this.replaceImportStringLiteral(node.source);
     }
     return super.visitExportNamedDeclaration(node);
   }
@@ -47,5 +31,17 @@ export class VendorVisitor extends Visitor {
     node.source.raw = `"${node.source.value}"`;
 
     return super.visitImportDeclaration(node);
+  }
+
+  private replaceImportStringLiteral(node: StringLiteral) {
+    const { value } = node;
+    const url = new URL(value);
+
+    node.value = `${prefix + hashFile(value.replace(url.origin, ""))}.js`;
+
+    //@ts-ignore StringLiteral missing raw field
+    node.raw = `"${node.value}"`;
+
+    return node;
   }
 }
