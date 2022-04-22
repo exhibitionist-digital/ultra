b006904d65ac65f7bc4e65ac85891167895deb72
jcmdln <jcmdln@gmail.com>
2022-04-21 20:00:19 -0400

    Add JSDoc comment for 'Cache' type
    Prior to this commit, the docs outline separately importing the
    'cache' type from './src/types.ts' which requires statically
    defining the import rather than adding it to 'importMap.json'.
    
    @mashaal suggested using JSDoc to make the 'Cache' type known on
    import of 'cache.js' which eliminates the need to separately import
    it.

--- cache.js
+++ cache.js
@@ -1,3 +1,8 @@
+/**
+ * @typedef {Map} Cache
+ * @param {Cache} cache
+ * @returns {Cache}
+ */
 const ultraCache = (cache) => {
   if (self.__ultra instanceof Array) return new Map(self.__ultra);
   else if (cache instanceof Map) return cache;
