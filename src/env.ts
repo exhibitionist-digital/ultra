061e57afaa0f54d3398d3fe19ef9cdd282cace67
James Edmonds <james@deckchairlabs.com>
2022-04-27 16:07:29 +0930

    Env validate (#103)
    * env validation
    
    * coerce with defaults
    
    * tests: added resolveEnv tests
    
    * test: legacy env vars
    
    * fix: port defaulted
    
    * fix: tests
    
    * fix: remove root

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,13 @@
-export const isDev = Deno.env.get("mode") === "dev";
-export const port = Deno.env.get("port") || 8000;
-export const sourceDirectory = Deno.env.get("source") || "src";
-export const vendorDirectory = Deno.env.get("vendor") || "x";
-export const apiDirectory = Deno.env.get("api") || "src/api";
-export const root = Deno.env.get("root") || `http://localhost:${port}`;
-export const lang = Deno.env.get("lang") || "en";
-export const disableStreaming = Deno.env.get("disableStreaming") || 0;
+import { resolveEnv } from "./resolveEnv.ts";
+
+const env = resolveEnv(Deno.env.toObject());
+
+export const isDev = env.mode === "dev";
+export const origin = env.origin;
+export const lang = env.lang;
+export const port = env.port;
+export const mode = env.mode;
+export const sourceDirectory = env.sourceDirectory;
+export const vendorDirectory = env.vendorDirectory;
+export const apiDirectory = env.apiDirectory;
+export const disableStreaming = env.disableStreaming;
