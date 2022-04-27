76d8602dd2c81b44905f27ba1f24904d6df20c17
James Edmonds <james@deckchairlabs.com>
2022-04-27 05:28:20 +0000

    fix: tests

--- src/resolveEnv.ts
+++ src/resolveEnv.ts
@@ -1,5 +1,6 @@
 import {
   boolean,
+  coerce,
   create,
   defaulted,
   Infer,
@@ -9,11 +10,20 @@ import {
   string,
 } from "https://esm.sh/superstruct@0.15.4";
 
+const portFromString = defaulted(
+  coerce(
+    number(),
+    string(),
+    (value) => Number(value),
+  ),
+  8000,
+);
+
 const envSchema = object({
   origin: string(),
   root: string(),
   lang: defaulted(string(), "en"),
-  port: defaulted(number(), 8000),
+  port: portFromString,
   mode: nullable(string()),
   sourceDirectory: defaulted(string(), "src"),
   vendorDirectory: defaulted(string(), "x"),
@@ -25,7 +35,7 @@ export type UltraEnvironment = Infer<typeof envSchema>;
 
 export function resolveEnv(env?: { [index: string]: string }) {
   const mode = env?.ULTRA_MODE || env?.mode || null;
-  const port = Number(env?.PORT || env?.port) || undefined;
+  const port = create(env?.PORT || env?.port, portFromString);
   const sourceDirectory = env?.ULTRA_SRC || env?.source;
   const vendorDirectory = env?.ULTRA_VENDOR || env?.vendor;
   const apiDirectory = env?.ULTRA_API_SRC || env?.api;
