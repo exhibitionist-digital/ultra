b1461a5b92815b634614f46fcade0d2cde9c2c66
James Edmonds <james@deckchairlabs.com>
2022-04-27 05:03:26 +0000

    tests: added resolveEnv tests

--- src/env.ts
+++ src/env.ts
@@ -1,62 +1,6 @@
-import {
-  boolean,
-  create,
-  defaulted,
-  Infer,
-  nullable,
-  number,
-  object,
-  string,
-} from "https://esm.sh/superstruct@0.15.4";
+import { resolveEnv } from "./resolveEnv.ts";
 
-const envSchema = object({
-  origin: string(),
-  root: string(),
-  lang: defaulted(string(), "en"),
-  port: defaulted(number(), 8000),
-  mode: nullable(string()),
-  sourceDirectory: defaulted(string(), "src"),
-  vendorDirectory: defaulted(string(), "x"),
-  apiDirectory: defaulted(string(), "src/api"),
-  disableStreaming: defaulted(boolean(), false),
-});
-
-export type UltraEnvironment = Infer<typeof envSchema>;
-
-export function resolveEnv() {
-  const denoEnv = Deno.env.toObject();
-
-  const mode = denoEnv.ULTRA_MODE || denoEnv.mode || null;
-  const port = Number(denoEnv.PORT || denoEnv.port) || 8000;
-  const sourceDirectory = denoEnv.ULTRA_SRC || denoEnv.source;
-  const vendorDirectory = denoEnv.ULTRA_VENDOR || denoEnv.vendor;
-  const apiDirectory = denoEnv.ULTRA_API_SRC || denoEnv.api;
-
-  const origin = denoEnv.ULTRA_ORIGIN || denoEnv.root ||
-    `http://localhost:${port}`;
-
-  const lang = denoEnv.ULTRA_LOCALE || denoEnv.lang;
-  const disableStreaming = denoEnv.disableStreaming;
-
-  const env = {
-    origin,
-    root: origin,
-    port,
-    mode,
-    sourceDirectory,
-    vendorDirectory,
-    apiDirectory,
-    lang,
-    disableStreaming,
-  };
-
-  /**
-   * Assert that the environment is valid and return the coerced/resolved value
-   */
-  return create(env, envSchema);
-}
-
-const env = resolveEnv();
+const env = resolveEnv(Deno.env.toObject());
 
 export const isDev = env.mode === "dev";
 export const origin = env.origin;
---
+++ src/resolveEnv.test.ts
@@ -0,0 +1,43 @@
+import { resolveEnv } from "./resolveEnv.ts";
+import { assertEquals } from "./deps.dev.ts";
+
+Deno.test("resolveEnv", async (t) => {
+  await t.step("defaults are correct", () => {
+    const env = resolveEnv();
+
+    assertEquals(env, {
+      apiDirectory: "src/api",
+      disableStreaming: false,
+      lang: "en",
+      mode: null,
+      origin: "http://localhost:8000",
+      port: 8000,
+      root: "http://localhost:8000",
+      sourceDirectory: "src",
+      vendorDirectory: "x",
+    });
+  });
+
+  await t.step("ULTRA_ prefixes", () => {
+    const env = resolveEnv({
+      ULTRA_ORIGIN: "https://example.com",
+      ULTRA_MODE: "test",
+      ULTRA_SRC: "source",
+      ULTRA_VENDOR: "vendor",
+      ULTRA_API_SRC: "cool/api",
+      ULTRA_LOCALE: "simlish",
+    });
+
+    assertEquals(env, {
+      origin: "https://example.com",
+      root: "https://example.com",
+      mode: "test",
+      port: 8000,
+      disableStreaming: false,
+      sourceDirectory: "source",
+      vendorDirectory: "vendor",
+      apiDirectory: "cool/api",
+      lang: "simlish",
+    });
+  });
+});
