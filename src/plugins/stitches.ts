591af17c3551a72d3624876243850b56a7587fdb
James Edmonds <james@deckchairlabs.com>
2022-05-13 05:42:32 +0000

    wip: stitches plugin

---
+++ src/plugins/stitches.ts
@@ -0,0 +1,32 @@
+import type { Plugin, ResponseTransformer } from "../deps.ts";
+import { isGetRequest, isHtmlResponse } from "../utils.ts";
+
+type PluginOptions = {
+  getCssText: () => string;
+};
+
+export const stitchesPlugin: Plugin<PluginOptions> = (app, options) => {
+  app.addResponseTransformer(
+    createResponseTransformer(options.getCssText),
+  );
+};
+
+function createResponseTransformer(
+  getCssText: () => string,
+): ResponseTransformer {
+  return function responseTransformer(
+    response,
+    context,
+    rewriter,
+  ) {
+    if (isGetRequest(context.request) && isHtmlResponse(response)) {
+      rewriter.on("head", {
+        element(element) {
+          element.append(`<style id="stitches">${getCssText()}</style>`, {
+            html: true,
+          });
+        },
+      });
+    }
+  };
+}
