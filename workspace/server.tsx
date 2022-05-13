c845ae698e780abd1df1e102c5bbe34a099a4c5f
James Edmonds <james@deckchairlabs.com>
2022-05-13 05:39:30 +0000

    wip: wouter

--- workspace/server.tsx
+++ workspace/server.tsx
@@ -1,10 +1,15 @@
+import { Router } from "wouter";
 import createServer from "../server.ts";
 import { reactHelmetPlugin } from "../src/plugins/react-helmet.ts";
 import { ServerAppProps } from "../src/types.ts";
 import App from "./src/app.tsx";
 
 function ServerApp({ state }: ServerAppProps) {
-  return <App state={state} />;
+  return (
+    <Router hook={staticLocationHook(state.url.pathname)}>
+      <App state={state} />
+    </Router>
+  );
 }
 
 const server = await createServer(ServerApp, {
@@ -13,5 +18,28 @@ const server = await createServer(ServerApp, {
 });
 
 server.register(reactHelmetPlugin);
-
 server.start({ port: 8000 });
+
+type Navigate = (to: string, opts?: { replace?: boolean }) => void;
+
+function staticLocationHook(
+  path = "/",
+  { record = false } = {},
+) {
+  // deno-lint-ignore prefer-const
+  let hook: { history?: string[] } & (() => [string, Navigate]);
+
+  const navigate: Navigate = (to, { replace } = {}) => {
+    if (record) {
+      if (replace) {
+        hook.history?.pop();
+      }
+      hook.history?.push(to);
+    }
+  };
+
+  hook = () => [path, navigate];
+  hook.history = [path];
+
+  return hook;
+}
