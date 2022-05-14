92d425659212ecea9b458b08ca6b91a79f991679
James Edmonds <james@deckchairlabs.com>
2022-05-14 08:09:10 +0000

    simplify

--- workspace/client.tsx
+++ workspace/client.tsx
@@ -2,12 +2,9 @@ import { hydrateRoot } from "react-dom/client";
 import { Ultra } from "@ultra/react";
 import App from "./src/app.tsx";
 
-function BrowserApp() {
-  return (
-    <Ultra>
-      <App state={window.__ultra_renderState} />
-    </Ultra>
-  );
-}
-
-hydrateRoot(document, <BrowserApp />);
+hydrateRoot(
+  document,
+  <Ultra>
+    <App state={window.__ultra_renderState} />
+  </Ultra>,
+);
