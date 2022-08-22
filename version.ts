8d4a559f0a1f1990e57252a85c8224449b830435
James Edmonds <james@deckchairlabs.com>
2022-08-22 14:35:26 +0000

    fix: added publish hooks

--- version.ts
+++ version.ts
@@ -1,1 +1,19 @@
 export const VERSION = "2.0.0-alpha.0";
+
+/** `prepublish` will be invoked before publish, return `false` to prevent the publish. */
+export async function prepublish(version: string) {
+  const readme = await Deno.readTextFile("./README.md");
+
+  await Deno.writeTextFile(
+    "./README.md",
+    readme.replace(
+      /\/\/deno\.land\/x\/ultra@v[\d\.]+\//,
+      `//deno.land/x/ultra@v${version}/`,
+    ),
+  );
+}
+
+/** `postpublish` will be invoked after published. */
+export function postpublish(version: string) {
+  console.log("Upgraded to", version);
+}
