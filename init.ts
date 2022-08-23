b0e0b07c44f88172c59cdaac7bfd0f741d2dcbd6
mashaal <omar@exhibitionist.digital>
2022-08-23 19:43:21 +1000

    update init page title

--- init.ts
+++ init.ts
@@ -300,7 +300,7 @@ function helloUltraContent() {
         <html lang="en">
           <head>
             <meta charSet="utf-8" />
-            <title>basic</title>
+            <title>Ultra</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="stylesheet" href="/style.css" />
