ba147b907f5df31b7828ad069b4616e9eeef01e4
James Edmonds <james@deckchairlabs.com>
2022-08-24 07:20:02 +0000

    wip: netlify build plugin

--- examples/with-netlify/build.ts
+++ examples/with-netlify/build.ts
@@ -1,8 +1,33 @@
-import build from "ultra/build.ts";
+import { ensureDir, join, outdent } from "../../lib/build/deps.ts";
+import build from "../../build.ts";
 
-const result = await build({
+await build({
   browserEntrypoint: import.meta.resolve("./client.tsx"),
   serverEntrypoint: import.meta.resolve("./server.tsx"),
-});
+  plugin: {
+    name: "netlify-edge",
+    async onBuild(result) {
+      console.log(result);
 
-console.log(result);
+      const outputDir = join(result.paths.rootDir, ".netlify");
+      const edgeFunctionsDir = join(outputDir, "edge-functions");
+
+      await ensureDir(outputDir);
+      await ensureDir(edgeFunctionsDir);
+
+      await Deno.writeTextFile(
+        join(edgeFunctionsDir, "server.ts"),
+        `export default () => new Response("Hello world");`,
+      );
+
+      await Deno.writeTextFile(
+        join(outputDir, "netlify.toml"),
+        outdent`
+          [[edge_functions]]
+          path = "/*"
+          function = "server"
+        `,
+      );
+    },
+  },
+});
