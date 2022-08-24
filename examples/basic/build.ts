946cfb60bebdfa3f4a0bdc10f295ed2d8e8a9007
James Edmonds <james@deckchairlabs.com>
2022-08-24 12:34:07 +0000

    chore: cleanup example

--- examples/basic/build.ts
+++ examples/basic/build.ts
@@ -1,8 +1,6 @@
 import build from "ultra/build.ts";
 
-const result = await build({
+await build({
   browserEntrypoint: import.meta.resolve("./client.tsx"),
   serverEntrypoint: import.meta.resolve("./server.tsx"),
 });
-
-console.log(result);
