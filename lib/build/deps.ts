26dd92d1ea5cb15e389083cc4c7e79f7a20a7920
James Edmonds <james@deckchairlabs.com>
2022-08-24 03:34:12 +0000

    fix: updated publish script

--- lib/build/deps.ts
+++ lib/build/deps.ts
@@ -1,5 +1,6 @@
 export { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
 export { default as outdent } from "https://deno.land/x/outdent@v0.8.0/mod.ts";
+export { globToRegExp } from "https://deno.land/std@0.152.0/path/glob.ts";
 export {
   brightBlue,
   green,
--- version.ts
+++ version.ts
@@ -1,16 +1,49 @@
+import { globToRegExp, walk } from "./lib/build/deps.ts";
+
 export const VERSION = "2.0.0-alpha.0";
+export const VERSION_REGEX = /\/\/deno\.land\/x\/ultra@v[\w\.\-]+\//;
+
+export const ULTRA_OUTPUT_REGEX = globToRegExp("examples/**/.ultra", {
+  extended: true,
+  globstar: true,
+  caseInsensitive: false,
+});
+
+export const IMPORT_MAP_REGEX = globToRegExp("examples/**/importMap.json", {
+  extended: true,
+  globstar: true,
+  caseInsensitive: false,
+});
 
 /** `prepublish` will be invoked before publish, return `false` to prevent the publish. */
 export async function prepublish(version: string) {
+  const newDenoLandVersion = `//deno.land/x/ultra@v${version}/`;
+
   const readme = await Deno.readTextFile("./README.md");
 
+  console.log("Patching README.md");
   await Deno.writeTextFile(
     "./README.md",
     readme.replace(
-      /\/\/deno\.land\/x\/ultra@v[\d\.]+\//,
+      VERSION_REGEX,
       `//deno.land/x/ultra@v${version}/`,
     ),
   );
+
+  console.log("Patching examples importMaps");
+
+  for await (
+    const entry of walk("./", {
+      match: [IMPORT_MAP_REGEX],
+      skip: [ULTRA_OUTPUT_REGEX],
+    })
+  ) {
+    const content = await Deno.readTextFile(entry.path);
+    await Deno.writeTextFile(
+      entry.path,
+      content.replace(VERSION_REGEX, newDenoLandVersion),
+    );
+  }
 }
 
 /** `postpublish` will be invoked after published. */
