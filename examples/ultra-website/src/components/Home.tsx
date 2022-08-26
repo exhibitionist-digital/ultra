5d941c19ca3ea882c9fbf13f1a6030ae21a33bce
Omar Mashaal <omar@teacups.io>
2022-08-26 18:57:48 +1000

    update alpha url

--- examples/ultra-website/src/components/Home.tsx
+++ examples/ultra-website/src/components/Home.tsx
@@ -34,7 +34,7 @@ export default function HomePage() {
           </p>
           <pre>
             <code>
-            deno run -A https://deno.land/x/ultra/init.ts
+            deno run -A https://deno.land/x/ultra@v2.0.0-alpha.5/init.ts
             </code>
           </pre>
 
