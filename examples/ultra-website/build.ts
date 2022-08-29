4f345c5a8ce7edc5e371deb1be15839a1c011563
James Edmonds <james@deckchairlabs.com>
2022-08-29 07:04:01 +0000

    examples: exclude

--- examples/ultra-website/build.ts
+++ examples/ultra-website/build.ts
@@ -3,4 +3,9 @@ import build from "ultra/build.ts";
 await build({
   browserEntrypoint: import.meta.resolve("./client.tsx"),
   serverEntrypoint: import.meta.resolve("./server.tsx"),
+  exclude: [
+    "./README.md",
+    "./fly.toml",
+    "./Dockerfile",
+  ],
 });
