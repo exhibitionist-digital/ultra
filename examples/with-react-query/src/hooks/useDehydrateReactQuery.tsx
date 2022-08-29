77df540aa8374c50d236832089f179554cf493ed
Omar Mashaal <omar@teacups.io>
2022-08-29 22:20:23 +1000

    add notes arount HTML escaping dehydrated state

--- examples/with-react-query/src/hooks/useDehydrateReactQuery.tsx
+++ examples/with-react-query/src/hooks/useDehydrateReactQuery.tsx
@@ -20,3 +20,9 @@ export function useDehydrateReactQuery(queryClient: QueryClient) {
     );
   });
 }
+
+// Note: If storing formatted HTML in your dehydrated state, it will need to be escaped, examples below.
+// https://github.com/exhibitionist-digital/ultra/issues/134
+// https://github.com/jaydenseric/ruck/blob/da7fb7d445128c22a32bd2f1fbf6fd5e1bc12886/jsonToRawHtmlScriptValue.mjs
+// https://github.com/vercel/next.js/blob/0af3b526408bae26d6b3f8cab75c4229998bf7cb/packages/next/server/htmlescape.ts
+// https://github.com/zertosh/htmlescape
