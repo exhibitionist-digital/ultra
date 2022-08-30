0b72324ae56c5737c09a5f0f95afea80860bc528
Omar Mashaal <omar@teacups.io>
2022-08-30 13:21:54 +1000

    clean up routing and use generateStaticHTML

--- examples/bogus-marketing-or-blog/server.tsx
+++ examples/bogus-marketing-or-blog/server.tsx
@@ -1,4 +1,3 @@
-import { serve } from "https://deno.land/std@0.153.0/http/server.ts";
 import { createServer } from "ultra/server.ts";
 import App from "./src/app.tsx";
 
@@ -8,27 +7,15 @@ const server = await createServer({
 });
 
 /**
- * Home route
- */
-server.get("/", async (context) => {
-  /**
-   * Render the request
-   */
-  const result = await server.render(<App request={context.req} />);
-
-  return context.body(result, 200, {
-    "content-type": "text/html",
-  });
-});
-
-/**
- * 404 fallback route
+ * Default route
  */
 server.get("*", async (context) => {
   /**
    * Render the request
    */
-  const result = await server.render(<App request={context.req} />);
+  const result = await server.render(<App request={context.req} />, {
+    generateStaticHTML: true,
+  });
 
   return context.body(result, 200, {
     "content-type": "text/html",
