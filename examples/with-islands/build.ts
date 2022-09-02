70ff75cafb47b27bf0dc62efbafcbed085c04b84
James Edmonds <james@deckchairlabs.com>
2022-09-02 05:10:10 +0000

    fix: remove client entrypoint

--- examples/with-islands/build.ts
+++ examples/with-islands/build.ts
@@ -1,11 +1,15 @@
 import { createBuilder } from "ultra/build.ts";
 
 const builder = createBuilder({
-  browserEntrypoint: import.meta.resolve("./client.tsx"),
   serverEntrypoint: import.meta.resolve("./server.tsx"),
 });
 
 builder.ignore("./README.md");
+
+builder.entrypoint("./src/app.tsx", {
+  vendorOutputDir: "browser",
+  target: "browser",
+});
 
 // deno-lint-ignore no-unused-vars
 const result = await builder.build();
--- examples/with-islands/client.tsx
+++
@@ -1,4 +0,0 @@
-import { hydrateRoot } from "react-dom/client";
-import App from "./src/app.tsx";
-
-hydrateRoot(document, <App />);
