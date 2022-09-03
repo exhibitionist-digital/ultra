40e819ac6ee5827c3ac0a8f26f8a8301dd0213bd
Omar Mashaal <omar@teacups.io>
2022-09-03 21:30:51 +1000

    feat: with-earthstar example

---
+++ examples/with-earthstar/build.ts
@@ -0,0 +1,14 @@
+import { createBuilder } from "ultra/build.ts";
+
+const builder = createBuilder({
+  browserEntrypoint: import.meta.resolve("./client.tsx"),
+  serverEntrypoint: import.meta.resolve("./server.tsx"),
+});
+
+builder
+  .ignore("./README.md")
+  .ignore("./fly.toml")
+  .ignore("./DockerFile");
+
+// deno-lint-ignore no-unused-vars
+const result = await builder.build();
