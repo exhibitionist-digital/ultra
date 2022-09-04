79ddd7a0ce946e0e7329220b87af42dfa72f67de
James Edmonds <james@deckchairlabs.com>
2022-09-04 09:28:58 +0000

    fix: linting

--- test/fixture/build.test.ts
+++ test/fixture/build.test.ts
@@ -1,7 +1,7 @@
-import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+// import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
 import builder from "./build.ts";
 
-Deno.test("it works", async (t) => {
+Deno.test("it works", async () => {
   const result = await builder.build();
   console.log(result);
 });
