886aa2f9d9d849b77cee8135a55ff614d0353467
James Edmonds <james@deckchairlabs.com>
2022-09-06 01:43:38 +0000

    wip

--- examples/with-netlify-(WIP)/build.ts
+++ examples/with-netlify-(WIP)/build.ts
@@ -1,5 +1,5 @@
 import { createBuilder } from "ultra/build.ts";
-import { netlify } from "ultra/lib/build/plugins/netlify.ts";
+import { netlify } from "../../lib/build/plugins/netlify.ts";
 
 const builder = createBuilder({
   browserEntrypoint: import.meta.resolve("./client.tsx"),
--- lib/build/plugins/netlify.ts
+++ lib/build/plugins/netlify.ts
@@ -1,8 +1,50 @@
+import { dirname, fromFileUrl, join } from "../../deps.ts";
+import { ensureDir } from "../deps.ts";
 import type { BuildPlugin } from "../types.ts";
 
 export const netlify: BuildPlugin = {
   name: "netlify-edge",
-  onBuild(_builder, _result) {
-    throw new Error("TODO");
+  async onBuild(builder) {
+    const __dirname = join(
+      dirname(fromFileUrl(new URL(import.meta.url))),
+      "netlify",
+    );
+
+    const dotEdgeFunctionsDir = join(
+      builder.context.output,
+      ".netlify",
+      "edge-functions",
+    );
+
+    const edgeFunctionsDir = join(
+      builder.context.output,
+      "netlify",
+      "edge-functions",
+    );
+
+    await ensureDir(dotEdgeFunctionsDir);
+    await ensureDir(edgeFunctionsDir);
+
+    await Deno.copyFile(
+      join(__dirname, "server.js"),
+      join(edgeFunctionsDir, "index.js"),
+    );
+
+    await Deno.writeTextFile(
+      join(dotEdgeFunctionsDir, "manifest.json"),
+      JSON.stringify(
+        {
+          functions: [
+            {
+              function: "index",
+              pattern: "^[^.]*$",
+            },
+          ],
+          version: 1,
+        },
+        null,
+        2,
+      ),
+    );
   },
 };
