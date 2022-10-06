f6fd27d983cdcf54318aa7fcaf6e21218aceb755
James Edmonds <james@deckchairlabs.com>
2022-10-06 06:23:39 +0000

    deps: updated mesozoic

--- examples/react-expermimental/src/app.tsx
+++ examples/react-expermimental/src/app.tsx
@@ -17,9 +17,9 @@ export default function App() {
         <meta charSet="utf-8" />
         <title>react-experimental</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href={useAsset("./favicon.ico")} />
-        <link rel="preload" as="style" href={useAsset("./style.css")} />
-        <link rel="stylesheet" href={useAsset("./style.css")} />
+        <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
+        <link rel="preload" as="style" href={useAsset("/style.css")} />
+        <link rel="stylesheet" href={useAsset("/style.css")} />
       </head>
       <body>
         <main>
