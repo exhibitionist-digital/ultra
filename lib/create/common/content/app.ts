7adfd10bfc176369bf793c27ba84d468754c3d6c
Omar Mashaal <omar@teacups.io>
2022-11-03 21:06:12 +1100

    chore: add content to create app

--- lib/create/common/content/app.ts
+++ lib/create/common/content/app.ts
@@ -38,6 +38,17 @@ export function appContent(config: Config) {
               <strong>Ultra</strong>. This is a barebones starter for your web
               app.
             </p>
+            <p>
+              Take{" "}
+              <a
+                href="https://ultrajs.dev/docs"
+                target="_blank"
+              >
+                this
+              </a>, you may need it where you are going. It will show you how to
+              customise your routing, data fetching, and styling with popular
+              libraries.
+            </p>
           </main>
         </body>
       </html>
