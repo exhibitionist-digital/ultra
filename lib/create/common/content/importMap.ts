fa789e3ecdba9fc09e02da6bffbb14bba6cfcc24
Omar Mashaal <omar@teacups.io>
2022-11-11 21:57:23 +1100

    2.1.0

--- lib/create/common/content/importMap.ts
+++ lib/create/common/content/importMap.ts
@@ -51,7 +51,7 @@ export function importMapContent(config: Config) {
     )
   }
 
-            "ultra/": "https://deno.land/x/ultra@v2.0.1/"
+            "ultra/": "https://deno.land/x/ultra@v2.1.0/"
          }
     }
    `;
