dc6cf1f9f710fd7399378f21687aace242cd929d
Benton Boychuk-Chorney <93301057+B3nten@users.noreply.github.com>
2022-12-07 19:22:40 -0700

    feat: add lite example (#224)
    * feat: add lite example
    
    * chore: formatting fix

---
+++ examples/lite/server.tsx
@@ -0,0 +1,26 @@
+import { serve } from "https://deno.land/std@0.164.0/http/server.ts";
+import { createServer } from "ultra/server.ts";
+import App from "./app.tsx";
+import React from "react";
+
+const server = await createServer({
+  importMapPath: import.meta.resolve("./importMap.json"),
+  browserEntrypoint: import.meta.resolve("./app.tsx"),
+});
+
+server.get("*", async (context) => {
+  /**
+   * Render the request
+   */
+  const result = await server.render(<App />);
+
+  return context.body(result, 200, {
+    "content-type": "text/html; charset=utf-8",
+  });
+});
+
+if (import.meta.main) {
+  serve(server.fetch);
+}
+
+export default server;
