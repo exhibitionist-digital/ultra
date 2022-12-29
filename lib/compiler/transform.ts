229c80a249921991346d755ce677418188c24120
benten <ben@benten.ca>
2022-12-29 02:27:19 -0800

    Cleanup

--- lib/compiler/transform.ts
+++ lib/compiler/transform.ts
@@ -1,14 +1,8 @@
 import initSwc, {
   transform,
 } from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
-// import { transform } from "https://deno.land/x/swc@0.2.1/mod.ts";
-import { cache } from "https://deno.land/x/cache@0.2.13/mod.ts";
 import { TransformSourceOptions } from "../types.ts";
-import { toFileUrl } from "../deps.ts";
 
-// const file = await cache(
-//   "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web_bg.wasm",
-// );
 await initSwc("https://esm.sh/@swc/wasm-web@1.3.11/wasm-web_bg.wasm");
 
 export async function transformSource(
