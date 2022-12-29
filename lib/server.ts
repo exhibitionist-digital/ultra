0ac2fb702bc3d3aabea8f18690b4cc8bd3373de2
benten <ben@benten.ca>
2022-12-29 00:31:24 -0800

    remove dynamic compiler import

--- lib/server.ts
+++ lib/server.ts
@@ -6,6 +6,7 @@ import { serveStatic } from "./middleware/serveStatic.ts";
 import { CreateServerOptions, Mode } from "./types.ts";
 import { UltraServer } from "./ultra.ts";
 import { resolveImportMapPath } from "./utils/import-map.ts";
+import { compiler } from "./middleware/compiler.ts";
 
 /**
  * Dotenv
@@ -75,7 +76,7 @@ export async function createServer(
 
   if (mode === "development") {
     log.info("Loading compiler");
-    const { compiler } = await import("./middleware/compiler.ts");
+    
 
     // deno-fmt-ignore
     server.get(`${ULTRA_COMPILER_PATH}/*`, compiler({
