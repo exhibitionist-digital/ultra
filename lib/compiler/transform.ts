8f38b0e3b8f3411384235da4bc9b59de50fc1d30
benten <ben@benten.ca>
2023-01-13 23:57:12 -0700

    feat: Test without swc wasm

--- lib/compiler/transform.ts
+++ lib/compiler/transform.ts
@@ -1,20 +1,5 @@
-import init, {
-  transform,
-} from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
-import { cache } from "https://deno.land/x/cache@0.2.13/mod.ts";
+import { transform } from "https://deno.land/x/swc@0.2.1/mod.ts";
 import { TransformSourceOptions } from "../types.ts";
-import { toFileUrl } from "../deps.ts";
-
-let file;
-if (Deno.env.get("ULTRA_MODE") === "development") {
-  file = await cache("https://esm.sh/@swc/wasm-web@1.3.11/wasm-web_bg.wasm");
-}
-
-await init(
-  file
-    ? toFileUrl(file.path)
-    : "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web_bg.wasm",
-);
 
 export async function transformSource(
   source: string,
