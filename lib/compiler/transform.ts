e9f72a410e45f43e4b5611165f6b3969f6a92883
benten <ben@benten.ca>
2023-01-14 01:48:08 -0700

    chore: re-add wasm

--- lib/compiler/transform.ts
+++ lib/compiler/transform.ts
@@ -1,26 +1,25 @@
-// import init, {
-//   transform,
-// } from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
-import { transform } from "https://deno.land/x/swc@0.2.1/mod.ts";
+import init, {
+  transform,
+} from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
 import { TransformSourceOptions } from "../types.ts";
 
-// let swcReady = false
-// async function ensureSWCInitialized() {
-//     if(swcReady) return
-//     if (Deno.run === undefined) {
-//       const wasmURL = new URL(import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) + "/wasm-web_bg.wasm", import.meta.url).href;
-//       const r = await fetch(wasmURL)
-//       const resp = new Response(r.body, {
-//         headers: { "Content-Type": "application/wasm" },
-//       });
-//       const wasmModule = await WebAssembly.compileStreaming(resp);
-//       await init(wasmModule);
-//       swcReady = true
-//     } else {
-//       await init(import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) + "/wasm-web_bg.wasm")
-//       swcReady = true
-//     }
-// }
+let swcReady = false
+async function ensureSWCInitialized() {
+    if(swcReady) return
+    if (Deno.run === undefined) {
+      const wasmURL = new URL(import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) + "/wasm-web_bg.wasm", import.meta.url).href;
+      const r = await fetch(wasmURL)
+      const resp = new Response(r.body, {
+        headers: { "Content-Type": "application/wasm" },
+      });
+      const wasmModule = await WebAssembly.compileStreaming(resp);
+      await init(wasmModule);
+      swcReady = true
+    } else {
+      await init(import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) + "/wasm-web_bg.wasm")
+      swcReady = true
+    }
+}
 
 export async function transformSource(
   source: string,
@@ -40,7 +39,7 @@ export async function transformSource(
     refresh = false,
   } = options;
 
-  // await ensureSWCInitialized();
+  await ensureSWCInitialized();
   const transformed = await transform(source, {
     // @ts-ignore This exists in the Rust API, but isn't exposed on the config type for some reason
     filename,
