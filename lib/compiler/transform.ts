08d81a26de990dfef497c99696ef00e8d85d06fc
benten <ben@benten.ca>
2023-01-18 22:58:15 -0700

    feat: replace swc with sucrase

--- lib/compiler/transform.ts
+++ lib/compiler/transform.ts
@@ -1,25 +1,5 @@
-import init, {
-  transform,
-} from "https://esm.sh/@swc/wasm-web@1.3.11/wasm-web.js";
 import { TransformSourceOptions } from "../types.ts";
-
-let swcReady = false
-async function ensureSWCInitialized() {
-    if(swcReady) return
-    if (Deno.run === undefined) {
-      const wasmURL = new URL(import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) + "/wasm-web_bg.wasm", import.meta.url).href;
-      const r = await fetch(wasmURL)
-      const resp = new Response(r.body, {
-        headers: { "Content-Type": "application/wasm" },
-      });
-      const wasmModule = await WebAssembly.compileStreaming(resp);
-      await init(wasmModule);
-      swcReady = true
-    } else {
-      await init(import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) + "/wasm-web_bg.wasm")
-      swcReady = true
-    }
-}
+import { transform } from "https://esm.sh/sucrase";
 
 export async function transformSource(
   source: string,
@@ -39,37 +19,14 @@ export async function transformSource(
     refresh = false,
   } = options;
 
-  await ensureSWCInitialized();
-  const transformed = await transform(source, {
-    // @ts-ignore This exists in the Rust API, but isn't exposed on the config type for some reason
-    filename,
-    jsc: {
-      target,
-      parser: {
-        syntax: "typescript",
-        dynamicImport,
-        tsx: true,
-      },
-      externalHelpers,
-      minify: minify
-        ? {
-          mangle: true,
-          compress: true,
-        }
-        : undefined,
-      transform: {
-        react: {
-          useBuiltins,
-          importSource: jsxImportSource,
-          runtime,
-          development,
-          refresh,
-        },
-      },
-    },
-    sourceMaps: sourceMaps ? true : undefined,
-    inlineSourcesContent: true,
-    minify,
+  const transformed = transform(source, {
+    jsxRuntime: "automatic",
+    jsxImportSource: "react",
+    transforms: [
+      "jsx",
+      "typescript",
+    ],
+    production: true,
   });
 
   /**
