a68d2c530f662662b622e0f0e58d65465d362bfd
James Edmonds <james@deckchairlabs.com>
2023-02-07 07:11:56 +0000

    fix: revert changes to basic/app.tsx

--- examples/basic/src/app.tsx
+++ examples/basic/src/app.tsx
@@ -1,3 +1,4 @@
+import useAsset from "ultra/hooks/use-asset.js";
 import useEnv from "ultra/hooks/use-env.js";
 
 export default function App() {
@@ -9,11 +10,34 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Ultra</title>
+        <title>basic</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
+        <link rel="preload" as="style" href={useAsset("/style.css")} />
+        <link rel="stylesheet" href={useAsset("/style.css")} />
       </head>
       <body>
-        <h1>Yep, hello</h1>
+        <main>
+          <h1>
+            <span></span>__<span></span>
+          </h1>
+          <p>
+            Welcome to{" "}
+            <strong>Ultra</strong>. This is a barebones starter for your web
+            app.
+          </p>
+          <p>
+            Take{" "}
+            <a
+              href="https://ultrajs.dev/docs"
+              target="_blank"
+            >
+              this
+            </a>, you may need it where you are going. It will show you how to
+            customise your routing, data fetching, and styling with popular
+            libraries.
+          </p>
+        </main>
       </body>
     </html>
   );
