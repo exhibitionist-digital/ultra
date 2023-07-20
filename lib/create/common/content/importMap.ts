c0d83e1bebb3680eea134ee3ce181a933edf20b9
James Edmonds <james@deckchairlabs.com>
2023-07-20 23:14:31 +0000

    chore: bump version

--- lib/create/common/content/importMap.ts
+++ lib/create/common/content/importMap.ts
@@ -62,7 +62,7 @@ export function importMapContent(config: Config) {
     )
   }
 
-            "ultra/": "https://deno.land/x/ultra@v2.3.2/"
+            "ultra/": "https://deno.land/x/ultra@v2.3.3/"
          }
     }
    `;
