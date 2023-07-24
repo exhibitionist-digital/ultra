ff2543796281cfe22ce3220d992805d1e6a227a1
James Edmonds <james@deckchairlabs.com>
2023-07-24 04:38:34 +0000

    fix: ultra mode is public

---
+++ test/unit/use-env.test.tsx
@@ -0,0 +1,46 @@
+import { assertEquals } from "https://deno.land/std@0.176.0/testing/asserts.ts";
+import useEnv from "../../hooks/use-env.js";
+import { renderToStream } from "../../lib/render.ts";
+
+Deno.test("useEnv hook", async () => {
+  let value;
+
+  Deno.env.set("ULTRA_MODE", "foo");
+
+  const App = () => {
+    value = useEnv("ULTRA_MODE");
+    return (
+      <html>
+        <head>
+          <title>useContext</title>
+        </head>
+        <body>
+          <div>{value}</div>
+        </body>
+      </html>
+    );
+  };
+
+  const stream = await renderToStream(
+    <App />,
+    undefined,
+    {
+      baseUrl: "/",
+      importMap: { imports: {} },
+      assetManifest: new Map(),
+    },
+  );
+
+  const response = new Response(stream);
+  const text = await response.text();
+
+  assertEquals(
+    text.includes("foo"),
+    true,
+  );
+
+  assertEquals(
+    text.includes("globalThis.__ULTRA_ENV"),
+    true,
+  );
+});
